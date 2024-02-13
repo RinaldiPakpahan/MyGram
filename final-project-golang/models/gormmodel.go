1c42bfcb55abb8bd7083bc211506680915296519
Rinaldi Pakpahan <rinaldi.pakpahan@gmail.com>
2024-02-13 19:48:04 +0700

    Delete final-project-golang directory
    Salah upload

--- final-project-golang/models/gormmodel.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "time"
-
-type GormModel struct {
-	ID       uint       `grom:"primaryKey" json:"id"`
-	CreateAt *time.Time `json:"created_at,omitempty"`
-	UpdateAt *time.Time `json:"updated_at,omitempty"`
-}
