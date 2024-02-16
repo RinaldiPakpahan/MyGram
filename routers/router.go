b77a0c50dd76a1d25b14d1058d53ca2b6c649f04
Rinaldi Pakpahan <rinaldi.pakpahan@gmail.com>
2024-02-16 18:05:15 +0700

    Update router.go
    Final Code

--- routers/router.go
+++ routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"final-project-golang/controllers"
+	"final-project-golang/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,11 +10,46 @@ import (
 func StartApp() *gin.Engine {
 	r := gin.Default()
 
+	//URL untuk manage user
 	userRouter := r.Group("/users")
 	{
 		userRouter.POST("/register", controllers.UserRegister)
-
 		userRouter.POST("/login", controllers.UserLogin)
+		userRouter.PUT("/update", middleware.Authentication(), controllers.UpdateUser)
+		userRouter.DELETE("/delete", middleware.Authentication(), controllers.DeleteUser)
+	}
+
+	//URL untuk manage social media
+	socialmediaRouter := r.Group("/socialmedia")
+	{
+		socialmediaRouter.Use(middleware.Authentication())
+		socialmediaRouter.POST("/create", controllers.CreateSocialMedia)
+		socialmediaRouter.GET("/", controllers.GetSocialMedia)
+		socialmediaRouter.GET("/:socialmediaId", controllers.GetIdSocialMedia)
+		socialmediaRouter.PUT("/:socialMediaId", middleware.SocialMediaAuthorization(), controllers.UpdateSocialMedia)
+		socialmediaRouter.DELETE("/:socialMediaId", middleware.SocialMediaAuthorization(), controllers.DeleteSocialMedia)
+	}
+
+	//URL untuk manage photo
+	photoRouter := r.Group("/photos")
+	{
+		photoRouter.Use(middleware.Authentication())
+		photoRouter.POST("/create", controllers.CreatePhoto)
+		photoRouter.GET("/", controllers.GetPhoto)
+		photoRouter.GET("/:photoId", controllers.GetByIdPhoto)
+		photoRouter.PUT("/:photoId", middleware.PhotoAuthorization(), controllers.UpdatePhoto)
+		photoRouter.DELETE("/:photoId", middleware.PhotoAuthorization(), controllers.DeletePhoto)
+	}
+
+	//URL untuk manage comment
+	commentRouter := r.Group("/comments")
+	{
+		commentRouter.Use(middleware.Authentication())
+		commentRouter.POST("/create", controllers.CreateComment)
+		commentRouter.GET("/", controllers.GetComment)
+		commentRouter.GET("/:commentId", controllers.GetByIdComment)
+		commentRouter.PUT("/:commentId", middleware.CommentAuthorization(), controllers.UpdateComment)
+		commentRouter.DELETE("/:commentId", middleware.CommentAuthorization(), controllers.DeleteComment)
 	}
 
 	return r
