76ecec13d576586299f25deebc629b145fe156dc
Rinaldi Pakpahan <rinaldi.pakpahan@gmail.com>
2024-02-16 18:02:14 +0700

    Update socialmediaControllers.go
    Final Code

--- controllers/socialmediaControllers.go
+++ controllers/socialmediaControllers.go
@@ -1,1 +1,183 @@
 package controllers
+
+import (
+	"encoding/json"
+	"final-project-golang/database"
+	"final-project-golang/helpers"
+	"final-project-golang/models"
+	"net/http"
+	"strconv"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+// Create Sosial Media
+func CreateSocialMedia(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	contentType := helpers.GetContentType(c)
+
+	socialMediaRequest := models.CreateSocialMediaRequest{}
+	userID := uint(userData["id"].(float64))
+	//pengecekan jenis content type yang digunakan
+	if contentType == appJson {
+		if err := c.ShouldBindJSON(&socialMediaRequest); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":   "Bad Request",
+				"message": err.Error(),
+			})
+			return
+		}
+	} else {
+		if err := c.ShouldBind(&socialMediaRequest); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":   "Bad Request",
+				"message": err.Error(),
+			})
+			return
+		}
+	}
+
+	socialMedia := models.SocialMedia{
+		Name:           socialMediaRequest.Name,
+		SocialMediaUrl: socialMediaRequest.SocialMediaUrl,
+		UserId:         userID,
+	}
+
+	err := db.Debug().Create(&socialMedia).Error
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	socialMediaString, _ := json.Marshal(socialMedia)
+	socialMediaResponse := models.CreateSocialMediaResponse{}
+	json.Unmarshal(socialMediaString, &socialMediaResponse)
+
+	c.JSON(http.StatusCreated, socialMediaResponse)
+}
+
+// Get All sosial media yang sudah dicreate
+func GetSocialMedia(c *gin.Context) {
+	db := database.GetDB()
+
+	socialMedias := []models.SocialMedia{}
+
+	err := db.Debug().Preload("User").Order("id asc").Find(&socialMedias).Error
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	socialMediaString, _ := json.Marshal(socialMedias)
+	socialMediaResponse := []models.SocialMediaResponse{}
+	json.Unmarshal(socialMediaString, &socialMediaResponse)
+
+	c.JSON(http.StatusOK, socialMediaResponse)
+}
+
+// GET sosial media by ID
+func GetIdSocialMedia(c *gin.Context) {
+	db := database.GetDB()
+	socialMediaId := c.Param("socialmediaId")
+	var allSocialMedia []models.SocialMedia
+
+	err := db.First(&allSocialMedia, "Id = ?", socialMediaId).Error
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Record not found",
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"Data": allSocialMedia,
+	})
+}
+
+// Update sosial media existing
+func UpdateSocialMedia(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	contentType := helpers.GetContentType(c)
+
+	socialMediaRequest := models.UpdateSocialMediaRequest{}
+	socialMediaId, _ := strconv.Atoi(c.Param("socialMediaId"))
+	userID := uint(userData["id"].(float64))
+	//pengecekan jenis content type yang digunakan
+	if contentType == appJson {
+		if err := c.ShouldBindJSON(&socialMediaRequest); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":   "Bad Request",
+				"message": err.Error(),
+			})
+			return
+		}
+	} else {
+		if err := c.ShouldBind(&socialMediaRequest); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":   "Bad Request",
+				"message": err.Error(),
+			})
+			return
+		}
+	}
+
+	socialMedia := models.SocialMedia{}
+	socialMedia.ID = uint(socialMediaId)
+	socialMedia.UserId = userID
+
+	updateString, _ := json.Marshal(socialMediaRequest)
+	updateData := models.SocialMedia{}
+	json.Unmarshal(updateString, &updateData)
+
+	//Update data social media hanya pada name dan social media url
+	err := db.Model(&socialMedia).Updates(updateData).Error
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+	_ = db.First(&socialMedia, socialMedia.ID).Error
+
+	socialMediaString, _ := json.Marshal(socialMedia)
+	socialMediaResponse := models.UpdateSocialMediaResponse{}
+	json.Unmarshal(socialMediaString, &socialMediaResponse)
+
+	c.JSON(http.StatusOK, socialMediaResponse)
+}
+
+// Delete Sosial Media existing
+func DeleteSocialMedia(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+
+	socialMediaId, _ := strconv.Atoi(c.Param("socialMediaId"))
+	userID := uint(userData["id"].(float64))
+
+	socialMedia := models.SocialMedia{}
+	socialMedia.ID = uint(socialMediaId)
+	socialMedia.UserId = userID
+
+	//Delete social media sesuai ID yang direquest
+	err := db.Delete(&socialMedia).Error
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Your social media has been successfully deleted",
+	})
+}
