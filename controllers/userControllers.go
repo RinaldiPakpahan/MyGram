a74613fa1518af1958df91211ea7561d664e5041
Rinaldi Pakpahan <rinaldi.pakpahan@gmail.com>
2024-02-16 18:02:33 +0700

    Update userControllers.go
    Final Code

--- controllers/userControllers.go
+++ controllers/userControllers.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"final-project-golang/database"
 	"final-project-golang/helpers"
 	"final-project-golang/models"
 	"net/http"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,12 +15,13 @@ var (
 	appJson = "Application/json"
 )
 
+// User Register
 func UserRegister(c *gin.Context) {
 	db := database.GetDB()
 	contentType := helpers.GetContentType(c)
 	_, _ = db, contentType
 	User := models.User{}
-
+	//pengecekan jenis content type yang digunakan
 	if contentType == appJson {
 		c.ShouldBindJSON(&User)
 	} else {
@@ -35,20 +38,22 @@ func UserRegister(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusCreated, gin.H{
-		"id":       User.ID,
-		"email":    User.Email,
-		"username": User.Username,
-		"age":      User.Age,
+		"id":              User.ID,
+		"email":           User.Email,
+		"username":        User.Username,
+		"age":             User.Age,
+		"urlImageProfile": User.ProfileImageUrl,
 	})
 }
 
+// User Login
 func UserLogin(c *gin.Context) {
 	db := database.GetDB()
 	contentType := helpers.GetContentType(c)
 	_, _ = db, contentType
 	User := models.User{}
 	password := ""
-
+	//pengecekan jenis content type yang digunakan
 	if contentType == appJson {
 		c.ShouldBindJSON(&User)
 	} else {
@@ -56,6 +61,7 @@ func UserLogin(c *gin.Context) {
 	}
 	password = User.Password
 
+	//Pengecekan email dan password untuk proses login
 	err := db.Debug().Where("email = ? ", User.Email).Take(&User).Error
 	if err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{
@@ -75,5 +81,83 @@ func UserLogin(c *gin.Context) {
 	token := helpers.GenerateToken(User.ID, User.Email)
 	c.JSON(http.StatusOK, gin.H{
 		"token": token,
+	})
+}
+
+// Update user existing
+func UpdateUser(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+	contentType := helpers.GetContentType(c)
+
+	//Hanya update email dan username saja
+	userRequest := models.UpdateUserRequest{}
+	userID := uint(userData["id"].(float64))
+	//pengecekan jenis content type yang digunakan
+	if contentType == appJson {
+		if err := c.ShouldBindJSON(&userRequest); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":   "Bad Request",
+				"message": err.Error(),
+			})
+			return
+		}
+	} else {
+		if err := c.ShouldBind(&userRequest); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":   "Bad Request",
+				"message": err.Error(),
+			})
+			return
+		}
+	}
+
+	user := models.User{}
+	user.ID = userID
+
+	updateString, _ := json.Marshal(userRequest)
+	updateData := models.User{}
+	json.Unmarshal(updateString, &updateData)
+
+	err := db.Model(&user).Updates(updateData).Error
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+	_ = db.First(&user, user.ID).Error
+
+	userString, _ := json.Marshal(user)
+	userResponse := models.CreateUserResponse{}
+	json.Unmarshal(userString, &userResponse)
+
+	//Akan ditampilkan email dan username yang sudah diupdate
+	c.JSON(http.StatusCreated, userResponse)
+}
+
+// Delete user existing
+func DeleteUser(c *gin.Context) {
+	db := database.GetDB()
+	userData := c.MustGet("userData").(jwt.MapClaims)
+
+	userID := uint(userData["id"].(float64))
+
+	user := models.User{}
+	user.ID = userID
+
+	//Delete user yang sudah diregister
+	err := db.Delete(&user).Error
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Your account has been successfully deleted",
 	})
 }
